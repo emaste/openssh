482c478ea96b4849b3916942534153a96483823a
Dag-Erling Smørgrav <des@FreeBSD.org>
2002-06-29 10:51:56 +0000

    Apply FreeBSD's configuration defaults.
    Sponsored by:	DARPA, NAI Labs
    
    (cherry picked from commit 95178965f189ef2f6534502fc7d1fb91378fdbaa)

--- readconf.c
+++ readconf.c
@@ -2133,7 +2133,7 @@ fill_default_options(Options * options)
 	if (options->batch_mode == -1)
 		options->batch_mode = 0;
 	if (options->check_host_ip == -1)
-		options->check_host_ip = 1;
+		options->check_host_ip = 0;
 	if (options->strict_host_key_checking == -1)
 		options->strict_host_key_checking = SSH_STRICT_HOSTKEY_ASK;
 	if (options->compression == -1)
