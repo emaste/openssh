741508280328dfcc4f7b0aaf3f7a5a7910667dcc
Konstantin Belousov <kib@FreeBSD.org>
2010-04-08 12:07:40 +0000

    Enhance r199804 by marking the daemonised child as immune to OOM instead of short-living parent. Only mark the master process that accepts connections, do not protect connection handlers spawned from inetd.
    Submitted by:	Mykola Dzham <i levsha me>
    Reviewed by:	attilio
    MFC after:	1 week
    
    (cherry picked from commit d93714f8812e363e9759b03b4ab5ce82fe55bdd1)

--- sshd.c
+++ sshd.c
@@ -46,6 +46,7 @@
 
 #include <sys/types.h>
 #include <sys/ioctl.h>
+#include <sys/mman.h>
 #include <sys/socket.h>
 #ifdef HAVE_SYS_STAT_H
 # include <sys/stat.h>
@@ -1983,6 +1984,10 @@ main(int ac, char **av)
 	}
 	/* Reinitialize the log (because of the fork above). */
 	log_init(__progname, options.log_level, options.log_facility, log_stderr);
+
+	/* Avoid killing the process in high-pressure swapping environments. */
+	if (!inetd_flag && madvise(NULL, 0, MADV_PROTECT) != 0)
+		debug("madvise(): %.200s", strerror(errno));
 
 	/* Chdir to the root directory so that the current disk can be
 	   unmounted if desired. */
