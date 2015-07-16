7a6775c17c1f4cd1adbe566d8bf6300e6263d837
Eric van Gyzen <vangyzen@FreeBSD.org>
2015-07-16 18:44:18 +0000

    ssh: canonicize the host name before looking it up in the host file
    Re-apply r99054 by des in 2002.  This was accidentally dropped
    by the update to OpenSSH 6.5p1 (r261320).
    
    This change is actually taken from r387082 of
    ports/security/openssh-portable/files/patch-ssh.c
    
    PR:		198043
    Differential Revision:	https://reviews.freebsd.org/D3103
    Reviewed by:	des
    Approved by:	kib (mentor)
    MFC after:	3 days
    Relnotes:	yes
    Sponsored by:	Dell Inc.
    
    (cherry picked from commit ccb9fcd6bb1f08a3b4a3ffc7f35e22675465e879)

--- ssh.c
+++ ssh.c
@@ -1385,6 +1385,23 @@ main(int ac, char **av)
 	cinfo->remuser = xstrdup(options.user);
 	cinfo->homedir = xstrdup(pw->pw_dir);
 	cinfo->locuser = xstrdup(pw->pw_name);
+	/* Find canonic host name. */
+	if (strchr(host, '.') == 0) {
+		struct addrinfo hints;
+		struct addrinfo *ai = NULL;
+		int errgai;
+		memset(&hints, 0, sizeof(hints));
+		hints.ai_family = options.address_family;
+		hints.ai_flags = AI_CANONNAME;
+		hints.ai_socktype = SOCK_STREAM;
+		errgai = getaddrinfo(host, NULL, &hints, &ai);
+		if (errgai == 0) {
+			if (ai->ai_canonname != NULL)
+				host = xstrdup(ai->ai_canonname);
+			freeaddrinfo(ai);
+		}
+	}
+
 
 	/*
 	 * Expand tokens in arguments. NB. LocalCommand is expanded later,
