126b55cd01c2516af578a74e5c37f483fa1f48f4
Ed Maste <emaste@FreeBSD.org>
2021-11-10 13:20:08 -0500

    Fix tracing disable on FreeBSD
    Some versions of FreeBSD do not support using id 0 to refer to the
    current pid for procctl, so pass getpid() explicitly.

--- platform-tracing.c
+++ platform-tracing.c
@@ -32,6 +32,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "log.h"
 
@@ -42,7 +43,7 @@ platform_disable_tracing(int strict)
 	/* On FreeBSD, we should make this process untraceable */
 	int disable_trace = PROC_TRACE_CTL_DISABLE;
 
-	if (procctl(P_PID, 0, PROC_TRACE_CTL, &disable_trace) && strict)
+	if (procctl(P_PID, getpid(), PROC_TRACE_CTL, &disable_trace) && strict)
 		fatal("unable to make the process untraceable: %s",
 		    strerror(errno));
 #endif
